aadebfac43e3aec240d2a94af7c73042d8da1c2e
youngmok.kim <indiemaru@gmail.com>
2026-02-12 16:43:53 +0900

    ✦ feat: 애플리케이션 전역 타임존 설정 기능 추가 (기본값: Asia/Seoul)

--- src/main/java/io/vision/api/VisionApplication.java
+++ src/main/java/io/vision/api/VisionApplication.java
@@ -1,10 +1,21 @@
 package io.vision.api;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VisionApplication {
+
+  @Value("${app.timezone}")
+  private String timezone;
+
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone(timezone));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(VisionApplication.class, args);
--- src/test/java/io/vision/api/VisionApplicationTests.java
+++ src/test/java/io/vision/api/VisionApplicationTests.java
@@ -2,9 +2,6 @@ package io.vision.api;
 
 import io.vision.api.support.IntegrationTestBase;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import org.springframework.test.context.TestPropertySource;
 
 class VisionApplicationTests extends IntegrationTestBase {
 
